1e02ddc882b606575a427abae7b13da5488b8732
MAKANI MBELOLO Belbeni Reille Kani <brk.makani@soprasteria.com>
2022-10-06 10:56:11 +0200

    esercizio completato

---
+++ src/GiorniMeseArray.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class GiorniMeseArray {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String meseArray[] = {"Gennaio","Febbraio","Marzo","Aprile","Maggio","Giugno","Luglio",
+				"Agosto","Settembre","Ottobre","Novembre","SDicembre"};
+		
+		int giorniMese[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+		
+		System.out.print("Inserire il numero del mese:");
+		int n = sc.nextInt();
+		
+		if(n <= 12) {
+		System.out.println("Hai inserito il numero "+n);
+		System.out.println("Mese "+meseArray[n-1]);
+		System.out.println("Gironi "+giorniMese[n-1]);
+		}else {
+			System.out.println("Hai inserito il numero compreso tra minore o uguale a 12");
+		}
+	}
+
+}
